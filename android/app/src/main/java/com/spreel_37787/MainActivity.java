b1cf16f31dc4865634b7de63ab52382705295712
Crowdbotics <team@crowdbotics.com>
2022-12-03 04:19:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/spreel_37787/MainActivity.java
@@ -0,0 +1,15 @@
+package com.spreel_37787;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "spreel_37787";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "spreel_37787",
+  url: "https://spreel_37787.botics.co",
+  api: "https://spreel_37787.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
